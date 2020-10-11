810b52262fa65b578b36e9daf0f635fd21f97763
sodiqlawal <sodiqlawal95@gmail.com>
2020-10-12 00:32:45 +0100

    Updates

---
+++ precache-manifest.b8a492fb3a439cd865a93615e2a53301.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fbbff9fd2f6e85f17f3805b5bcfb129",
+    "url": "/Lightinghaus/index.html"
+  },
+  {
+    "revision": "4486ff486669df10a011",
+    "url": "/Lightinghaus/static/css/main.b5611c91.chunk.css"
+  },
+  {
+    "revision": "17b2b4c0ee731a4d16d3",
+    "url": "/Lightinghaus/static/js/2.8275c140.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Lightinghaus/static/js/2.8275c140.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4486ff486669df10a011",
+    "url": "/Lightinghaus/static/js/main.dcdbca59.chunk.js"
+  },
+  {
+    "revision": "0beffef1222b0dac1cbe",
+    "url": "/Lightinghaus/static/js/runtime-main.99bc06c6.js"
+  },
+  {
+    "revision": "e212f84086965da44a6c84f3d9a683a4",
+    "url": "/Lightinghaus/static/media/Poppins-Regular.e212f840.ttf"
+  },
+  {
+    "revision": "36a3337cbbfa6ad3ebea30cc4b363ef5",
+    "url": "/Lightinghaus/static/media/app-store.36a3337c.svg"
+  },
+  {
+    "revision": "ae78f6bda260da31891779ae3f974b54",
+    "url": "/Lightinghaus/static/media/banner.ae78f6bd.jpeg"
+  },
+  {
+    "revision": "73fa9f3e3a134dec20a3e93217461c76",
+    "url": "/Lightinghaus/static/media/categoryOne.73fa9f3e.jpg"
+  },
+  {
+    "revision": "26f73524afa735fadca804a86443dc06",
+    "url": "/Lightinghaus/static/media/categoryTwo.26f73524.jpg"
+  },
+  {
+    "revision": "e7c3715aff9c7a7fd87f2a3f837593c8",
+    "url": "/Lightinghaus/static/media/google-play.e7c3715a.svg"
+  },
+  {
+    "revision": "03cf966488ec8a412084974706cc2ba2",
+    "url": "/Lightinghaus/static/media/mailingImg.03cf9664.png"
+  },
+  {
+    "revision": "3772f441d40e4ff0512cc840005bbcca",
+    "url": "/Lightinghaus/static/media/popularThree.3772f441.png"
+  },
+  {
+    "revision": "b3f4081bc75b19a6d6316f8050b072a1",
+    "url": "/Lightinghaus/static/media/popularTwo.b3f4081b.jpg"
+  },
+  {
+    "revision": "60b58a763ed4d5159e358e489e9e392e",
+    "url": "/Lightinghaus/static/media/spaceThree.60b58a76.jpg"
+  },
+  {
+    "revision": "02c8ad065e89a190b04a776612a71854",
+    "url": "/Lightinghaus/static/media/spaceTwo.02c8ad06.jpg"
+  }
+]);
